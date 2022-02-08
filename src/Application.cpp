e89fd03a6a1782a068a251842f1e594b80bd029d
PXIONG001 <pengxiong001@gmail.com>
2022-02-07 22:07:14 -0600

    First Commit.

---
+++ src/Application.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : Application.cpp
+// Author      : Peng Xiong
+// Version     : 1.0
+// Copyright   : Your copyright notice
+// Description : Bank Management Application will be the main
+//============================================================================
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+	cout << "Hello World!" << endl;
+	return 0;
+}
